d2da56219e6f7e3e68825d8f0709818fc40dde7a
Zett-8 <kame0008@gmail.com>
2020-05-21 16:42:21 +0200

    fix

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const express = require('express')
 const httpProxy = require('http-proxy')
 const app = express()
 
+const PORT = 3333
+const frontend = 8888
+
 switch (process.env.NODE_ENV) {
   case 'production':
     break
 
   default:
     const proxy = httpProxy.createProxyServer({ target: 'http://localhost:3333' })
-    proxy.listen(8888)
+    proxy.listen(frontend)
 }
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.listen(3333, () => console.log(`running on 3333`))
+app.listen(PORT, () => console.log(`Listening to 3333\nalso navigate 8888 -> 3333`))
